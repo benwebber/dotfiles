0209a530362a69405a29c0ebd42e52472c061c71
Ben Webber <benjamin.webber@gmail.com>
2014-07-29 21:34:56 -0400

    Add Python SMTP and HTTP server aliases

--- .bashrc.d/aliases.bash
+++ .bashrc.d/aliases.bash
@@ -8,7 +8,11 @@ alias :e='vim'
 alias :q='exit'
 alias histstat='history | awk "{print $4}" | sort | uniq -c | sort -rn | head'
 alias mount='mount | column -t'
+
+# Simple (s*) commands
 alias sdig='dig +noall +answer'
+alias shttpd='python -m SimpleHTTPServer'
+alias ssmtpd='python -m smtpd -n -c DebuggingServer localhost:1025'
 
 # coreutils
 alias cp='cp -ivr'
