b7b742ef5a6911dd26f22595476e12f8a9301cfa
Ben Webber <benjamin.webber@gmail.com>
2014-08-03 23:22:15 -0400

    Add bundled Go binaries to path

--- .bashrc.d/go.bash
+++ .bashrc.d/go.bash
@@ -4,5 +4,6 @@
 # Golang
 #-------------------------------------------------------------------------------
 
+export GOROOT=/usr/local/Cellar/go/1.3/libexec
 export GOPATH=~/projects/go
-export PATH=$GOPATH/bin:$PATH
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
