0ebb2ac5d0f244ecfe004819ea010a08a34388a1
Ben Webber <benjamin.webber@gmail.com>
2015-05-09 16:55:33 -0400

    Remove redundant completion search

--- bash/.bashrc.d/prompt.bash
+++ bash/.bashrc.d/prompt.bash
@@ -4,24 +4,6 @@
 # Completions and $PS1
 #------------------------------------------------------------------------------
 
-completion_dirs=(
-  '~/.bash_completion.d'
-  '/etc/bash_completion.d'
-  '/usr/local/etc/bash_completion.d'
-)
-completion_files=(
-  'git-prompt.sh'
-  'git-completion.bash'
-  'tig-completion.bash'
-)
-for cdir in "${completion_dirs[@]}"; do
-  for cfile in "${completion_files[@]}"; do
-    if [[ -f "${cdir}/${cfile}" ]]; then
-      . "${cdir}/${cfile}"
-    fi
-  done
-done
-
 . $(brew --prefix)/etc/bash_completion 2>&1 >/dev/null
 
 # Sets a typical PS1 including virtualenv and Git branch. Aligns PS2 (line
