40cb20c9df980e681edbee447fc3afc0c62a20be
Ben Webber <benjamin.webber@gmail.com>
2015-05-09 16:50:46 -0400

    Align PS2 with PS1

--- bash/.bashrc.d/prompt.bash
+++ bash/.bashrc.d/prompt.bash
@@ -24,10 +24,20 @@ done
 
 . $(brew --prefix)/etc/bash_completion 2>&1 >/dev/null
 
+# Sets a typical PS1 including virtualenv and Git branch. Aligns PS2 (line
+# continuation prompt) with $ of preceding PS1.
 __prompt() {
+  # Mimic \u, \h, and \w. Bash only evaluates these when rendering the prompt,
+  # so we cannot use them to calculate the PS2 width.
+  local u=${USER}
+  local h=${HOSTNAME%%.*}
+  local w=${PWD/$HOME\//\~/}
   local venv=
   [[ $VIRTUAL_ENV ]] && venv="[${VIRTUAL_ENV##*/}] "
-  PS1="${venv}\u@\h:\w$(__git_ps1)$ "
+  PS1="${venv}${u}@${h}:${w}$(__git_ps1)$ "
+  # Calculate PS2 width.
+  local w_ps1=${#PS1}
+  PS2=$(printf "%s" "$(printf " %.0s" $(eval "echo {1..$w_ps1}"))")
 }
 
 PROMPT_COMMAND=__prompt
