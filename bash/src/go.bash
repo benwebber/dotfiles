7288ab10d33262be5515fd03d5cbe9b464467352
Ben Webber <benjamin.webber@gmail.com>
2015-08-24 22:14:58 -0400

    Update Go to 1.4

--- bash/src/go.bash
+++ bash/src/go.bash
@@ -2,6 +2,6 @@
 # Golang
 #-------------------------------------------------------------------------------
 
-export GOROOT=/usr/local/Cellar/go/1.3/libexec
+export GOROOT=/usr/local/Cellar/go/1.4.2/libexec
 export GOPATH=~/projects/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
