ca546505d18b0c93415a4f62fa18fcb5a5767924
Ben Webber <benjamin.webber@gmail.com>
2016-08-21 23:10:06 -0400

    Do not set GOROOT (no longer necessary)

--- bash/src/go.bash
+++ bash/src/go.bash
@@ -2,8 +2,6 @@
 # Golang
 #-------------------------------------------------------------------------------
 
-export GOROOT=/usr/local/Cellar/go/1.4.2/libexec
 export GOPATH=~/src/go
 
-path put "${GOROOT}/bin"
 path put "${GOPATH}/bin"
