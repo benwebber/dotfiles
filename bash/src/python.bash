f1d8a1e4ab5962ad621828d1cc5f84a1f41e3f72
Ben Webber <benjamin.webber@gmail.com>
2016-10-28 21:10:32 -0400

    Do not write Python bytecode files

--- bash/src/python.bash
+++ bash/src/python.bash
@@ -8,6 +8,7 @@ export WORKON_HOME=$HOME/.virtualenvs
 export VIRTUALENVWRAPPER_SCRIPT="$(command -v virtualenvwrapper.sh)"
 export PIPSI_HOME=${WORKON_HOME}
 export PIPSI_BIN_DIR=${HOME}/bin
+export PYTHONDONTWRITEBYTECODE=1
 
 [[ -f $VIRTUALENVWRAPPER_SCRIPT ]] && . "${VIRTUALENVWRAPPER_SCRIPT/%.sh/_lazy.sh}"
 
