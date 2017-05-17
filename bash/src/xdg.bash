4ff69e1da68070643169bb5b763f38565951bf7e
Ben Webber <benjamin.webber@gmail.com>
2017-05-17 16:58:45 +0800

    bash :Add XDG base directory specification configuration

---
+++ bash/src/xdg.bash
@@ -0,0 +1,5 @@
+[[ -z $XDG_CACHE_HOME ]] && export XDG_CACHE_HOME="${HOME}/.cache"
+[[ -z $XDG_CONFIG_HOME ]] && export XDG_CONFIG_HOME="${HOME}/.config"
+[[ -z $XDG_DATA_HOME ]] && export XDG_DATA_HOME="${HOME}/.local/share"
+
+export LEDGER_FILE="${XDG_DATA_HOME}/hledger/hledger.journal"
