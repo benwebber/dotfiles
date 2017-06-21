6057c430022d5e77760d0c6f3b120b78f48a6c94
Ben Webber <benjamin.webber@gmail.com>
2017-06-21 06:00:37 +0000

    bash: Use tput instead of hard-coded ANSI escape sequences

--- bash/src/prompt.bash
+++ bash/src/prompt.bash
@@ -4,6 +4,10 @@
 
 . "${USR_PATH}/share/bash-completion/bash_completion" >/dev/null 2>&1
 
+COLOUR_BG_DARK_GREY="$(tput setab 8)"
+COLOUR_BG_LIGHT_GREEN="$(tput setab 10)"
+STYLE_RESET="$(tput sgr0)"
+
 __fossil_ps1() {
   local info
   local fmt=' (%s)'
@@ -28,12 +32,12 @@ __prompt() {
   history -a
   __duiker_import
   local venv=
-  [[ $VIRTUAL_ENV ]] && venv="\[\e[42m ${VIRTUAL_ENV##*/} \e[m\]"
+  [[ $VIRTUAL_ENV ]] && venv="\[${COLOUR_BG_LIGHT_GREEN} ${VIRTUAL_ENV##*/} ${STYLE_RESET}\]"
   local git_status fsl_status
   git_status="$(__git_ps1 ' git ᚠ %s ')"
-  [[ -n $git_status ]] && git_status="\[\e[100m${git_status}\e[m\]"
+  [[ -n $git_status ]] && git_status="\[${COLOUR_BG_DARK_GREY}${git_status}${STYLE_RESET}\]"
   fsl_status="$(__fossil_ps1 ' fsl ᚠ %s ')"
-  [[ -n $fsl_status ]] && fsl_status="\[\e[100m${fsl_status}\e[m\]"
+  [[ -n $fsl_status ]] && fsl_status="\[${COLOUR_BG_DARK_GREY}${fsl_status}${STYLE_RESET}\]"
   local status="${git_status}${fsl_status}${venv}"
   # Prefix user@host with space if there is status information to show, or if
   # we are using Bash 4.4+ with vi mode indicators.
