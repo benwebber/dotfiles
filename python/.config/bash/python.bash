f6e9cd0deec87f052332cfa841a2bd17eabaa490
Ben Webber <benjamin.webber@gmail.com>
2020-06-21 03:26:42 +0000

    Remove unused virtualenv configuration

--- python/.config/bash/python.bash
+++ python/.config/bash/python.bash
@@ -1,11 +1,8 @@
 #require os
 #require utils
 
-# Configure virtualenv
-export PROJECT_HOME=$HOME/src
 export WORKON_HOME=$HOME/.virtualenvs
 export VIRTUALENVWRAPPER_SCRIPT="$(command -v virtualenvwrapper.sh)"
-export PIPSI_HOME=${WORKON_HOME}
 export PYTHONDONTWRITEBYTECODE=1
 
 [[ -f $VIRTUALENVWRAPPER_SCRIPT ]] && . "${VIRTUALENVWRAPPER_SCRIPT/%.sh/_lazy.sh}"
